67f3e96f690388b67f8afc81e703f69c138163f9
Samantha Bast <basts@roverkids.org>
2025-04-16 13:49:05 -0400

    Finished 2.2

--- AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
@@ -8,10 +8,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -20,7 +22,7 @@ public class PlayerController : MonoBehaviour
         // Keep player in bounds
         if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(- xRange, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
 
         if (transform.position.x > xRange)
@@ -29,5 +31,12 @@ public class PlayerController : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
+
 }
