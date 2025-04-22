ad77499d9885a92ecee739ac158823c14e9192f0
Samantha Bast <basts@roverkids.org>
2025-04-22 14:09:30 -0400

    Completed 2.4

--- AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
+++ AnimalStampede/AnimalStampede/Assets/Scripts/PlayerController.cs
@@ -6,7 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10.0f;
+    public float xRange = 15.0f;
 
     public GameObject projectilePrefab;
 
@@ -29,6 +29,8 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
