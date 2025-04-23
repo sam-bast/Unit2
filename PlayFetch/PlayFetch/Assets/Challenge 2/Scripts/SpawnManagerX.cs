160250199f7f9d67d9afd2eca7cffb0b63fba735
Samantha Bast <basts@roverkids.org>
2025-04-23 14:03:15 -0400

    Finished Challenge 2

--- PlayFetch/PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ PlayFetch/PlayFetch/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -6,9 +6,9 @@ public class SpawnManagerX : MonoBehaviour
 {
     public GameObject[] ballPrefabs;
 
-    private float spawnLimitXLeft = -22;
-    private float spawnLimitXRight = 7;
-    private float spawnPosY = 30;
+    private float spawnLimitXLeft = -22.0f;
+    private float spawnLimitXRight = 7.0f;
+    private float spawnPosY = 30.0f;
 
     private float startDelay = 1.0f;
     private float spawnInterval = 4.0f;
@@ -17,16 +17,20 @@ public class SpawnManagerX : MonoBehaviour
     void Start()
     {
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        // Selects ball for spawning
+        int randomBall = Random.Range(0, ballPrefabs.Length);
+
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[randomBall], spawnPos, ballPrefabs[0].transform.rotation);
     }
 
 }
